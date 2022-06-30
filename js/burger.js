4233733db79ea0b3180a40e921afe5220dea0668
Vadym-Vasiliev <98331150+Vadym-Vasiliev@users.noreply.github.com>
2022-06-30 12:49:30 +0300

    add menu burger

--- js/burger.js
+++ js/burger.js
@@ -9,6 +9,6 @@
     menuBtnRef.setAttribute('aria-expanded', !expanded);
 
     mobileMenuRef.classList.toggle('is-open');
-    document.body.classList.toggle('menu-burger');
+    document.body.classList.toggle('menu-open');
   });
 })();
